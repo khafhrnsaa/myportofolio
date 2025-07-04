de0bceb5a77cade11591c8028acab37f798e2a0f
khafhrnsaa <gaharuearn@gmail.com>
2025-07-04 10:58:10 +0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Kita akan buat file ini selanjutnya
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
